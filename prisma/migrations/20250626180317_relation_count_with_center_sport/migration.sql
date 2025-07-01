22627e9cf7d83a11942b67d99725120a3597908e
Kévin Lopes <contatokevinlopes@gmail.com>
2025-07-01 18:59:40 -0300

    Delete prisma/migrations directory
    Removendo migrations

--- prisma/migrations/20250626180317_relation_count_with_center_sport/migration.sql
+++
@@ -1,2 +0,0 @@
--- AddForeignKey
-ALTER TABLE "public"."court" ADD CONSTRAINT "court_sportCenterId_fkey" FOREIGN KEY ("sportCenterId") REFERENCES "public"."sport_center"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
